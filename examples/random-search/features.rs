a442bd03df207ff5748615bdeffe6922412c5591
Martin Renold <martin@log2.ch>
2021-08-08 21:23:07 +0200

    refactor: split out feature-calculation

---
+++ examples/random-search/features.rs
@@ -0,0 +1,56 @@
+use progenitor::{Cell, CellTypeRef, World};
+
+#[derive(Default, Clone, Copy)]
+struct FeatureAccumulator {
+    acc: i64,
+    n: u32,
+}
+
+impl FeatureAccumulator {
+    fn push(&mut self, value: i32) {
+        self.acc += value as i64;
+        self.n += 1;
+    }
+    fn push_weighted(&mut self, value: i32, weight: u16) {
+        self.acc += value as i64;
+        self.n += weight as u32;
+    }
+    fn merge((fa1, fa2): (Self, Self)) -> Self {
+        Self {
+            acc: fa1.acc + fa2.acc,
+            n: fa1.n + fa2.n,
+        }
+    }
+}
+
+impl From<FeatureAccumulator> for f64 {
+    fn from(fa: FeatureAccumulator) -> Self {
+        fa.acc as f64 / fa.n as f64
+    }
+}
+
+fn calculate_features(world: World) -> [FeatureAccumulator; 2] {
+    const EMPTY: CellTypeRef = CellTypeRef(1);
+    fn cell2int(c: Cell) -> i32 {
+        if c.cell_type == EMPTY { 0 } else { 1 }
+    }
+    let mut features = [FeatureAccumulator::default(); 2];
+    for (cell, neighbours) in world.iter_cells_with_neighbours() {
+        let center = cell2int(cell);
+        let neighbours: i32 = neighbours.iter().map(|(_, c)| cell2int(*c)).sum();
+        features[0].push(center);
+        features[1].push_weighted((neighbours - 6 * center).abs(), 6);
+    }
+    features
+}
+
+pub fn evaluate<F>(run: F, repetitions: i32) -> [f64; 2]
+where F: Fn() -> World
+{
+    (0..repetitions)
+        .map(|_| run())
+        .map(calculate_features)
+        .reduce(|a, b| a.zip(b).map(FeatureAccumulator::merge))
+        .unwrap()
+        .map(|fa| fa.into())
+}
--- examples/random-search/main.rs
+++ examples/random-search/main.rs
@@ -1,12 +1,14 @@
 #![feature(array_zip)]
 use progenitor::world1::Params;
-use progenitor::{Cell, CellTypeRef, World, world1};
+use progenitor::{World, world1};
 use rand::thread_rng;
 use rayon::prelude::*;
 
 use std::collections::HashMap;
 use std::fs::File;
 use std::io::prelude::*;
+
+mod features;
 
 fn run(params: &Params) -> World {
     let mut world = World::new();
@@ -16,60 +18,6 @@ fn run(params: &Params) -> World {
         world.tick();
     }
     world
-}
-
-#[derive(Default, Clone, Copy)]
-struct FeatureAccumulator {
-    acc: i64,
-    n: u32,
-}
-
-impl FeatureAccumulator {
-    fn push(&mut self, value: i32) {
-        self.acc += value as i64;
-        self.n += 1;
-    }
-    fn push_weighted(&mut self, value: i32, weight: u16) {
-        self.acc += value as i64;
-        self.n += weight as u32;
-    }
-    fn merge((fa1, fa2): (Self, Self)) -> Self {
-        Self {
-            acc: fa1.acc + fa2.acc,
-            n: fa1.n + fa2.n,
-        }
-    }
-}
-
-impl From<FeatureAccumulator> for f64 {
-    fn from(fa: FeatureAccumulator) -> Self {
-        fa.acc as f64 / fa.n as f64
-    }
-}
-
-const EMPTY: CellTypeRef = CellTypeRef(1);
-
-fn calculate_features(world: World) -> [FeatureAccumulator; 2] {
-    fn cell2int(c: Cell) -> i32 {
-        if c.cell_type == EMPTY { 0 } else { 1 }
-    }
-    let mut features = [FeatureAccumulator::default(); 2];
-    for (cell, neighbours) in world.iter_cells_with_neighbours() {
-        let center = cell2int(cell);
-        let neighbours: i32 = neighbours.iter().map(|(_, c)| cell2int(*c)).sum();
-        features[0].push(center);
-        features[1].push_weighted((neighbours - 6 * center).abs(), 6);
-    }
-    features
-}
-
-fn evaluate(params: &Params) -> [FeatureAccumulator; 2] {
-    let repetitions = 32;
-    (0..repetitions)
-        .map(|_| run(params))
-        .map(calculate_features)
-        .reduce(|a, b| a.zip(b).map(FeatureAccumulator::merge))
-        .unwrap()
 }
 
 fn main() {
@@ -97,7 +45,8 @@ fn main() {
     let bins_found = population
         .into_par_iter()
         .map(|params| {
-            let score: [f64; 2] = evaluate(&params).map(|v| v.into());
+            let repetitions = 32;
+            let score: [f64; 2] = features::evaluate(|| run(&params), repetitions);
             eprintln!("{:?}", params);
             println!("{:.6} {:.6}", score[0], score[1]);
             let world = run(&params);
@@ -117,16 +66,6 @@ fn main() {
         });
 
     eprintln!("found {} bins: {:?}", bins_found.len(), bins_found.keys());
-
-    /*
-    for (bin, world) in bins_found {
-        let filename = format!("output_{}_{}.dat", bin.0, bin.1);
-        eprintln!("found bin {:?}, writing {}", bin, filename);
-        let data = world.export_snapshot();
-        let mut file = File::create(&filename).unwrap();
-        file.write_all(&data).unwrap();
-    }
-    */
 
     let snapshots: Vec<((i32, i32), Vec<u8>)> = bins_found
         .into_iter()
