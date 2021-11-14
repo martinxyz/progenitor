962239652a41a23054bf09a0d2dd21f5ab790d91
Martin Renold <martin@log2.ch>
2021-11-14 20:07:40 +0100

    output file generated by --example random-search
    For preserving this as a mini-demo. Also, label the UI such that there
    is a small chance I will still know how to use it in a year.

--- www/src/RuleSelector.svelte
+++ www/src/RuleSelector.svelte
@@ -7,6 +7,6 @@
         <option value={'demo1'}>1 - simple pattern</option>
         <option value={'demo2'}>2 - progrenitor cells</option>
         <option value={'demo3'}>3 - noisy blobs</option>
-        <option value={'demo4'} selected>4 - experiment</option>
+        <option value={'demo4'} selected>4 - experiment results (click map above)</option>
     </select>
 </div>
