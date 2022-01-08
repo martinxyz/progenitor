708d204a408b536d55ee4c02fcc43a6e2b272a10
Martin Renold <martin@log2.ch>
2022-01-08 21:38:35 +0100

    refactor: extract TaskStream code into module

--- crates/progenitor/examples/map-elites/main.rs
+++ crates/progenitor/examples/map-elites/main.rs
@@ -4,13 +4,14 @@ use progenitor::{world1, World};
 use rand::prelude::IteratorRandom;
 use rand::{thread_rng, Rng};
 
-use std::collections::{HashMap, VecDeque};
+use std::collections::HashMap;
 use std::fs::{create_dir_all, File};
 use std::io::prelude::*;
 
 use crate::features::FEATURE_COUNT;
 
 mod features;
+mod taskstream;
 
 fn run(params: &Params) -> World {
     let mut world = World::new();
@@ -53,70 +54,34 @@ fn main() {
     let mut rng = thread_rng();
 
     let mut bins_found: HashMap<_, EvalResult> = HashMap::new();
-    rayon::in_place_scope(|s| {
-        // Without this max_tasks limit, we are ~6% slower. (Maybe something
-        // with rayon task order. But schedule_fifo() doesn't seem to help. Or
-        // maybe some AMD hyperthreading thing.) (2xVCPUs: 6% slower)
-        let max_tasks = num_cpus::get() * 1; // 1xVCPUs
 
-        let mut tasks = VecDeque::new();
-        let (results_s, results_r) = crossbeam::channel::unbounded();
-
-        // add initial tasks
-        for i in 0..POPULATION_LAG {
-            let params = sample_initial_params(&mut rng);
-            tasks.push_back((i, params));
+    let init: Vec<_> = (0..POPULATION_LAG)
+        .map(|_| sample_initial_params(&mut rng))
+        .collect();
+    let mut total_tasks = init.len();
+    taskstream::run_stream(init, process, |(i, eval_result)| {
+        let map_resolution = (0.05, 0.02);
+        let score = eval_result.0;
+        let bc1 = (score[0] / map_resolution.0).round() as i32;
+        let bc2 = (score[1] / map_resolution.1).round() as i32;
+        let bin = (bc1, bc2);
+        if bins_found.insert(bin, eval_result).is_none() {
+            eprintln!("evaluation {}: found {} bins", i, bins_found.len());
+            println!("{} {}", i, bins_found.len());
         }
 
-        // consume each result, add task that depend on this result
-        let mut ready_results = HashMap::new();
-        let mut tasks_scheduled = 0;
-        for i in 0..EVALUATIONS {
-            while !ready_results.contains_key(&i) {
-                // schedule more tasks
-                while tasks_scheduled < max_tasks && !tasks.is_empty() {
-                    tasks_scheduled += 1;
-                    let results_s = results_s.clone();
-                    let (i, params) = tasks.pop_front().unwrap();
-                    s.spawn(move |_| {
-                        results_s.send((i, process(params))).unwrap();
-                    });
-                }
-                // pop results
-                let (i, res) = results_r.recv().unwrap();
-                tasks_scheduled -= 1;
-                ready_results.insert(i, res);
-                if ready_results.len() > POPULATION_LAG / 2 {
-                    eprintln!(
-                        "Warning: LAG too low? ready_results / LAG: {} / {}",
-                        ready_results.len(),
-                        POPULATION_LAG
-                    );
-                }
-            }
-            let eval_result = ready_results.remove(&i).unwrap();
-
-            let map_resolution = (0.05, 0.02);
-            let score = eval_result.0;
-            let bc1 = (score[0] / map_resolution.0).round() as i32;
-            let bc2 = (score[1] / map_resolution.1).round() as i32;
-            let bin = (bc1, bc2);
-            if bins_found.insert(bin, eval_result).is_none() {
-                eprintln!("evaluation {}: found {} bins", i, bins_found.len());
-                println!("{} {}", i, bins_found.len());
-            }
-
-            let task_i = i + POPULATION_LAG;
-            if task_i < EVALUATIONS {
-                let random_parent = bins_found.values().choose(&mut rng).unwrap();
-                let mut params = random_parent.1.clone();
+        if total_tasks < EVALUATIONS {
+            total_tasks += 1;
+            let random_parent = bins_found.values().choose(&mut rng).unwrap();
+            let mut params = random_parent.1.clone();
+            params.mutate(&mut rng);
+            while rng.gen_bool(0.7) {
                 params.mutate(&mut rng);
-                while rng.gen_bool(0.7) {
-                    params.mutate(&mut rng);
-                }
-                // params = sample_initial_params(&mut rng);  // for validation: converges ~40% slower
-                tasks.push_back((task_i, params));
             }
+            // params = sample_initial_params(&mut rng);  // for validation: converges ~40% slower
+            Some(params)
+        } else {
+            None
         }
     });
     eprintln!("found {} bins: {:?}", bins_found.len(), bins_found.keys());
---
+++ crates/progenitor/examples/map-elites/taskstream.rs
@@ -0,0 +1,59 @@
+use std::collections::{HashMap, VecDeque};
+
+/// Run a stream of tasks in parallel, with dependencies.
+///
+/// process_task() is called from a thread pool (arbitrary order)
+///
+/// process_result() is called synchronously, in the same order that tasks were
+/// submitted. It may return a new task to submit.
+pub fn run_stream<T, R, FT, FR>(
+    initial_tasks: impl IntoIterator<Item = T>,
+    process_task: FT,
+    mut process_result: FR,
+) where
+    T: Send,
+    R: Send,
+    FT: Fn(T) -> R + Send + Copy,
+    FR: FnMut((usize, R)) -> Option<T>,
+{
+    rayon::in_place_scope(|s| {
+        // Without this max_tasks limit, we are ~6% slower. (Maybe something
+        // with rayon task order. But schedule_fifo() doesn't seem to help. Or
+        // maybe some AMD hyperthreading thing.) (2xVCPUs: 6% slower)
+        let max_pending_tasks = num_cpus::get(); // 1xVCPUs
+
+        let mut tasks: VecDeque<(usize, T)> = initial_tasks.into_iter().enumerate().collect();
+        let mut total_tasks_queued = tasks.len();
+        let (results_s, results_r) = crossbeam::channel::unbounded();
+
+        let mut results = HashMap::<usize, R>::new();
+        let mut tasks_pending = 0;
+
+        // Ugh. Simplify loop conditions?
+        let mut i = 0;
+        while !tasks.is_empty() || !results.is_empty() || tasks_pending > 0 {
+            while !results.contains_key(&i) {
+                // schedule more tasks
+                while tasks_pending < max_pending_tasks && !tasks.is_empty() {
+                    tasks_pending += 1;
+                    let results_s = results_s.clone();
+                    let (j, params) = tasks.pop_front().unwrap();
+                    s.spawn(move |_| {
+                        results_s.send((j, process_task(params))).unwrap();
+                    });
+                }
+                // pop one result
+                let (j, res) = results_r.recv().unwrap();
+                tasks_pending -= 1;
+                results.insert(j, res);
+            }
+            let result = results.remove(&i).unwrap();
+
+            if let Some(new_task) = process_result((i, result)) {
+                tasks.push_back((total_tasks_queued, new_task));
+                total_tasks_queued += 1;
+            }
+            i += 1;
+        }
+    });
+}
