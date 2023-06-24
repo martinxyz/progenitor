7f5e4ede592b11f552777767fd548042bd2373aa
Martin Renold <martin@log2.ch>
2023-06-24 20:55:05 +0200

    random symmetric look-up table rules

--- crates/progenitor/src/hexmap.rs
+++ crates/progenitor/src/hexmap.rs
@@ -1,3 +1,5 @@
+#![allow(dead_code)]
+
 use crate::{coords, AxialTile};
 
 pub struct Location {
--- crates/progenitor/src/tumblers.rs
+++ crates/progenitor/src/tumblers.rs
@@ -1,6 +1,6 @@
-use rand::distributions::Bernoulli;
-use rand::distributions::Distribution;
-use rand::prelude::SliceRandom;
+use std::array;
+
+use nalgebra::SVector;
 use rand::seq::IteratorRandom;
 use rand::thread_rng;
 use rand::Rng;
@@ -8,6 +8,7 @@ use rand::RngCore;
 use rand::SeedableRng;
 use serde::{Deserialize, Serialize};
 
+use crate::axial_tile::Neighbourhood;
 use crate::coords;
 use crate::coords::Direction;
 use crate::hexmap;
@@ -45,6 +46,75 @@ pub struct Tumblers {
     visited: AxialTile<Option<bool>>,
     rng: SimRng,
     tumble_prob: f64,
+    air_rule: AirRule,
+}
+
+const NEIGHBOURHOOD_FEATURE_LENGTH: usize = 4;
+
+#[derive(Clone, Serialize, Deserialize)]
+pub struct AirRule {
+    neighbourhood_features: [SVector<i8, NEIGHBOURHOOD_FEATURE_LENGTH>; 2 * 2 * 3],
+    neighbourhood_features_thresholds: [i8; NEIGHBOURHOOD_FEATURE_LENGTH],
+    transform: [bool; 1 << NEIGHBOURHOOD_FEATURE_LENGTH],
+    change_heading: [bool; 1 << NEIGHBOURHOOD_FEATURE_LENGTH],
+}
+
+impl AirRule {
+    fn new_random(rng: &mut impl Rng) -> Self {
+        Self {
+            neighbourhood_features: array::from_fn(|_| {
+                SVector::from_fn(|_, _| *[-13, -2, -1, 0, 0, 1, 2, 13].iter().choose(rng).unwrap())
+            }),
+            neighbourhood_features_thresholds: array::from_fn(|_| rng.gen_range(-60..=60)),
+            transform: array::from_fn(|_| rng.gen_bool(0.2)),
+            change_heading: array::from_fn(|_| rng.gen_bool(0.2)),
+        }
+    }
+
+    fn step(&self, neighbourhood: Neighbourhood<Cell>, rng: &mut impl Rng) -> Cell {
+        let mut features_sum: SVector<i8, NEIGHBOURHOOD_FEATURE_LENGTH> = SVector::zeros();
+
+        let center = neighbourhood.center;
+        #[allow(clippy::identity_op)]
+        for i in 0..6usize {
+            let dir = Direction::from_int(i as i32);
+            let neigh = neighbourhood.neighbours[i];
+            // let heading_result = match (neigh.center.heading, neigh.
+
+            let center_heads_to_neigh = center.heading.map(|dir2| dir2 == dir).unwrap_or(false);
+            let neigh_heads_to_center = neigh.heading.map(|dir2| dir2 == -dir).unwrap_or(false);
+            let mut idx = 0usize;
+            idx += 1 * neigh_heads_to_center as usize;
+            idx += 2 * center_heads_to_neigh as usize;
+            idx += 4 * match neigh.kind {
+                CellType::Air => 0,
+                CellType::Blob => 1,
+                CellType::Stone | CellType::Border => 2,
+            };
+            let feature = self.neighbourhood_features[idx];
+            features_sum += feature;
+            // features_max[i] = features_max[i].max(feature);
+        }
+        let mut idx = 0usize;
+        let mut pow = 1usize;
+        for (sum, threshold) in features_sum
+            .iter()
+            .zip(self.neighbourhood_features_thresholds)
+        {
+            if sum > &threshold {
+                idx += pow;
+            }
+            pow *= 2;
+        }
+        let mut cell = center;
+        if self.change_heading[idx] {
+            cell.heading = Direction::try_from((rng.gen::<u8>() % 8) as i32).ok();
+        }
+        if self.transform[idx] {
+            cell.kind = CellType::Blob;
+        }
+        cell
+    }
 }
 
 const RADIUS: i32 = 12;
@@ -95,13 +165,15 @@ impl Tumblers {
                     ..neighbourhood.center
                 };
             }
-            return neighbourhood.center;
+            neighbourhood.center
         });
+        let air_rule = AirRule::new_random(&mut rng);
         Tumblers {
             state,
             visited: hexmap::new(RADIUS, None, |_location| Some(false)),
             rng,
             tumble_prob,
+            air_rule,
         }
     }
 
@@ -123,6 +195,11 @@ impl Tumblers {
 impl Simulation for Tumblers {
     fn step(&mut self) {
         // let tumble_dist = Bernoulli::new(self.tumble_prob).unwrap();
+        self.state
+            .ca_step(|neighbourhood| match neighbourhood.center.kind {
+                CellType::Air => self.air_rule.step(neighbourhood, &mut self.rng),
+                _ => neighbourhood.center,
+            });
 
         for (cell, visited) in self.state.iter_cells().zip(self.visited.iter_cells_mut()) {
             *visited = visited.map(|visited| visited || cell.kind == CellType::Blob);
@@ -151,7 +228,6 @@ impl HexgridView for Tumblers {
             },
             direction: cell.heading,
             energy: visited.map(|v| if v { 1 } else { 0 }),
-            ..Default::default()
         })
     }
     fn cell_text(&self, pos: coords::Cube) -> Option<String> {
