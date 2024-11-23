1389dc48d87cf76f7a72a05a55e315d1368579bf
Martin Renold <martin@log2.ch>
2024-11-23 07:55:22 +0100

    webapp: better fix for the pixi.js interaction problem
    Thanks to @LunarRaid on the PixiJS discord for pointing me there.

--- webapp/vite.config.ts
+++ webapp/vite.config.ts
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
 import wasm from 'vite-plugin-wasm'
-import topLevelAwait from 'vite-plugin-top-level-await'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -9,10 +8,10 @@ export default defineConfig({
         // hot-reload breaks when the wasm module got recompiled
         svelte({ hot: false }),
         wasm(),
-        topLevelAwait(),
     ],
     base: '',
     build: {
+        target: 'es2022',
         minify: true,
         sourcemap: true,
     },
